48f8861c4b9dc106d8fe8b66a70c140b26e7a27a
panditdhamdhere <dhamdherepandit@gmail.com>
2023-08-31 17:46:32 +0530

    ready to deploy contract

--- web3/scripts/deploy.js
+++ web3/scripts/deploy.js
@@ -1,11 +1,17 @@
 const hre = require("hardhat");
 
 async function main() {
+  // STAKING CONTRACT
   const tokenStaking = await hre.ethers.deployContract("TokenStaking");
-
   await tokenStaking.waitForDeployment();
 
+  // TOKEN CONTRACT
+  const theblockchain = await hre.ethers.deployContract("TheBlockchain");
+  await theblockchain.waitForDeployment();
+
+  // CONTRACT ADDRESS
   console.log(` STACKING: ${tokenStaking.target}`);
+  console.log(` TOKEN: ${theblockchain.target}`);
 }
 
 main().catch((error) => {
